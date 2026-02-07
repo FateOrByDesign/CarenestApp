3e93ac93a101991c27920cdb05fe98bdaa1bbe83
savindu204 <savindu.20240043@iit.ac.lk>
2026-02-07 16:10:02 +0530

    Add mobile role selection and dashboards

--- Carenest_Frontend/carenest_mobile/ios/Flutter/flutter_export_environment.sh
+++ Carenest_Frontend/carenest_mobile/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\nethw\OneDrive\Documents\GitHub\CarenestApp\Carenest_Frontend\carenest_mobile"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\IIT\2nd Year\2nd sem\SDGP_git_codes\carenest_mobileapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- Carenest_Frontend/carenest_mobile/lib/main.dart
+++ Carenest_Frontend/carenest_mobile/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'screens/patient/patient_details.dart';
-import 'screens/caregiver/caregiver_details.dart';
-import 'screens/caregiver/caregiver_notification_page.dart';
+
+import 'core/role_storage.dart';
+import 'core/user_role.dart';
+
+import 'pages/role_select_page.dart';
+import 'pages/caregiver_dashboard_page.dart';
+import 'pages/carereceiver_dashboard_page.dart';
 
 void main() {
   runApp(const CareNestApp());
@@ -12,11 +16,48 @@ class CareNestApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CareNest',
-      theme: ThemeData(primarySwatch: Colors.teal),
-      home: PatientDetailsPage(),
+      home: _StartupGate(),
+    );
+  }
+}
+
+class _StartupGate extends StatelessWidget {
+  const _StartupGate();
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return FutureBuilder<UserRole?>(
+  //     future: RoleStorage.getRole(),
+  //     builder: (context, snapshot) {
+  //       // Loading state
+  //       if (snapshot.connectionState == ConnectionState.waiting) {
+  //         return const Scaffold(
+  //           body: Center(child: CircularProgressIndicator()),
+  //         );
+  //       }
+
+  //       final role = snapshot.data;
+
+  //       if (role == UserRole.caregiver) {
+  //         return const CaregiverDashboardPage();
+  //       }
+
+  //       if (role == UserRole.careReceiver) {
+  //         return const CareReceiverDashboardPage();
+  //       }
+
+  //       return const RoleSelectPage();
+  //     },
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RoleSelectPage(),
     );
   }
 }
